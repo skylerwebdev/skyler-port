3219eb081e4afc7df6b3e013c5d72caeccb68375
Skyler Dowdy <skylerwebdev@gmail.com>
2019-12-31 11:43:56 -0500

    commmmmmmit

--- src/components/pages/main/Projects.jsx
+++ src/components/pages/main/Projects.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "../../style/css/Project.css";
 const Projects = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="pageMain">
+      <div className="prMain">
+        <h1 className="header">My Projects</h1>
+        <div className="prTop">
+          <div className="prTopLt">
+            <img src="https://via.placeholder.com/150" alt="placeholder" />
+            <img src="https://via.placeholder.com/150" alt="placeholder" />
 
-export default Projects
+          </div>
+          <div className="prTopMd"></div>
+          <div className="prTopRt"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
